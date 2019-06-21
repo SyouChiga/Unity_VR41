56350382f884a7d6065557ff7c9b73957ad6f8ac
suzuki <szk32526113535j@gmail.com>
2019-06-21 11:54:55 +0900

    アセットの追加

---
+++ Public VR/Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Player : MonoBehaviour
+{
+    //var device = SteamVR_
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Controller()
+    {
+        
+    }
+}
