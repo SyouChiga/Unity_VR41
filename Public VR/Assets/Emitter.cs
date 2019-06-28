7177a2c8367da6efd6c6767a8c90a2a1314f3d63
taharayuta <taharayuta9658@gmail.com>
2019-06-28 11:14:56 +0900

    たぶん球が出る

--- Public VR/Assets/Emitter.cs
+++ Public VR/Assets/Emitter.cs
@@ -1,35 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+
 
 public class Emitter : MonoBehaviour
 {
 	public GameObject obj;
 	static float counter = 0;
+	SteamVR_TrackedObject trackedObj;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	//GameObject LeftController;
+	//GameObject  RightController;
 
-    // Update is called once per frame
-    void Update()
-    {
-		counter += Time.deltaTime;
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
 
-		if(counter > 3.0f)
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+
+	void Update()
+	{
+
+		var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
 		{
 
-			// Cubeプレハブを元に、インスタンスを生成、
-			Instantiate(obj, this.transform.position, Quaternion.identity);
+			GameObject Tracker = GameObject.Find("[CameraRig]");
 
-			counter = 0.0f;
+			GameObject LeftController = Tracker.transform.Find("Controller (left)").gameObject;
+
+			// Cubeプレハブを元に、インスタンスを生成、
+			GameObject Ball = Instantiate(obj, LeftController.transform.position, Quaternion.identity);
+
+			Ball.GetComponent<Rigidbody>().AddForce(this.transform.forward * 1000.0f);
+				
+			
 		}
 
 
 	}
 
-
 }
-
