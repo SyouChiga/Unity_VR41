d2ce7a89a9d15d2d883382e26f51164a8819d1b8
THS65104\nagata ryoya <naga-ryo0625@outlook.com>
2019-06-28 11:48:56 +0900

    test GunShot

---
+++ Public VR/Assets/Scripts/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Controller : MonoBehaviour
+{
+    // <summer></summer>
+    private SteamVR_Action_Boolean actionToHaptic = SteamVR_Actions._default.Menu;
+
+    //<summer></summer>
+    private SteamVR_Action_Vibration haptic = SteamVR_Actions._default.Haptic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(actionToHaptic.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            haptic.Execute(0, 0.005f, 0.005f, 1, SteamVR_Input_Sources.LeftHand);
+        }
+    }
+}
