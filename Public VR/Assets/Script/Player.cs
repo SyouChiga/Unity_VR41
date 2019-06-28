39d525ea3e57a4cbf7d0aeca521c952e1af13482
suzuki <szk32526113535j@gmail.com>
2019-06-28 11:58:03 +0900

    弾出ると思う

--- Public VR/Assets/Script/Player.cs
+++ Public VR/Assets/Script/Player.cs
@@ -6,6 +6,10 @@ using Valve.VR;
 public class Player : MonoBehaviour
 {
     //var device = SteamVR_
+    public SteamVR_Input_Sources hand;
+    public SteamVR_Action_Boolean isGrabAction;
+    [SerializeField] private GameObject bulletPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isGrabAction.GetState(SteamVR_Input_Sources.Any))
+        {
+            GameObject bullet = Instantiate<GameObject>(bulletPrefab);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward);
+            Destroy(bullet, 2.0f);
+        }
         
+
+
     }
 
     void Controller()
